49b36f01cd141ac38e3817413ca5b797731a11b0
Mateusz <mateusz@example.com>
2018-07-07 09:37:21 +0200

    Create working version of slides in sidebar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
 $(function () {
+    var isSidebarExpanded = false;
+    var countOpenMenus = 0;
+
     $('.sidebar').hover(function () {
-        $('.sidebar__btn-text, .sidebar__tab-header-title').animate({width: 'toggle'});
+        if (countOpenMenus === 0){
+            $('.toggleable')
+                .animate({width: 'toggle'})
+                .promise()
+                .then(function () {
+
+                });
         }
-    );
+        isSidebarExpanded = !isSidebarExpanded;
+
+    });
+
+    $('.sidebar__tab-menu').on('customSlideUp', function (e) {
+        $(this).slideUp();
+    });
+
+    $('.sidebar__tab-menu').on('customSlideDown', function (e) {
+        $(this).slideDown();
+    });
+
+    $('.sidebar__tab-header').click(function () {
+        var tabMenu = $(this).siblings('.sidebar__tab-menu');
+
+        if(isSidebarExpanded) {
+            console.log('log');
+            if (tabMenu.is(':hidden')){
+                tabMenu.trigger('customSlideDown');
+                countOpenMenus++;
+            }
+            else {
+                tabMenu.trigger('customSlideUp');
+                countOpenMenus--;
+            }
+        }
+
+
+    });
+
+    // $('.sidebar').trigger('mouseenter');
 });
