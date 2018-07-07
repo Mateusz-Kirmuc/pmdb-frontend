7b44de57168b1fc7c19061543ac84940734a52af
Mateusz <mateusz@example.com>
2018-07-07 09:54:51 +0200

    Refactor

--- js/script.js
+++ js/script.js
@@ -6,17 +6,19 @@ $(function () {
         if (countOpenMenus === 0){
             $('.toggleable')
                 .animate({width: 'toggle'})
-                .promise()
-                .then(function () {
-
-                });
         }
         isSidebarExpanded = !isSidebarExpanded;
 
     });
 
     $('.sidebar__tab-menu').on('customSlideUp', function (e) {
-        $(this).slideUp();
+        $(this).slideUp(function() {
+            countOpenMenus--;
+            if(!$('.sidebar:hover').length){
+                $('.sidebar').trigger('mouseleave');
+                isSidebarExpanded = !isSidebarExpanded;
+            }
+        });
     });
 
     $('.sidebar__tab-menu').on('customSlideDown', function (e) {
@@ -25,21 +27,14 @@ $(function () {
 
     $('.sidebar__tab-header').click(function () {
         var tabMenu = $(this).siblings('.sidebar__tab-menu');
-
         if(isSidebarExpanded) {
-            console.log('log');
             if (tabMenu.is(':hidden')){
                 tabMenu.trigger('customSlideDown');
                 countOpenMenus++;
             }
             else {
                 tabMenu.trigger('customSlideUp');
-                countOpenMenus--;
             }
         }
-
-
     });
-
-    // $('.sidebar').trigger('mouseenter');
 });
