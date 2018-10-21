3e4ef95b5a90bcc0930d26d0162ef2a2e9787b4d
mateusz <mateusz.kirmuc@gmail.com>
2018-10-21 11:40:18 +0200

    [refaktoring] Dodano task do gulpa z konkatenacją plików js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task('default', function () {
-   // code for default task here
+gulp.task('built:js', function () {
+   return gulp.src([
+       './js/_sidebar.js',
+       './js/_table.js',
+       './js/_side-menus.js'
+       ])
+       .pipe(concat('script.js'))
+       .pipe(gulp.dest('./static/js/'));
+
+
 });
